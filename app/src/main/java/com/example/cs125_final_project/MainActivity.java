7884f94f49188043598d0c04b0a703da8a5084df
hollylee <jungmin3@illinois.edu>
2020-04-27 09:49:15 +0900

    fixed FloatingActionButtion errors

--- app/src/main/java/com/example/cs125_final_project/MainActivity.java
+++ app/src/main/java/com/example/cs125_final_project/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.cs125_final_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
