dcd898c57393d8f04a7f59e6f33297f60653f947
dl-ux <dl14@illinois.edu>
2020-04-27 20:26:35 -0500

    Updated To Do

--- app/src/main/java/com/example/cs125_final_project/MainActivity.java
+++ app/src/main/java/com/example/cs125_final_project/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.cs125_final_project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //Creates list to add tasks
         task_list = findViewById(R.id.task_list);
         tasks = new ArrayList<>();
@@ -36,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         removeTask();
         //implements add
         addTask();
+        taskDescription();
     }
 
     // When held, the item is deleted (maybe change this to a button next to each task?)
@@ -61,9 +67,26 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent taskDesc = new Intent(getApplicationContext(), TaskDescriptionActivity.class);
-                startActivity(taskDesc);
+                EditText task = findViewById(R.id.editText);
+                final String taskOnClick = task.getText().toString();
+                if (taskOnClick != null && taskOnClick.length() != 0) {
+                    itemsAdapter.add(taskOnClick);
+                    itemsAdapter.notifyDataSetChanged();
+                    task.getText().clear();
+                }
             }
         });
+    }
+    // When held, the item is deleted (maybe change this to a button next to each task?)
+    private void taskDescription() {
+        task_list.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        Intent intent = new Intent(getApplicationContext(), TaskDescriptionActivity.class);
+                        startActivity(intent);
+                    }
+                });
     }
 }
