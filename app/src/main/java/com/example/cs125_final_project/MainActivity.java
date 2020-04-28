feaa06e3ba5b2b48b6187b65a831ca8c4ebae77e
hollylee <jungmin3@illinois.edu>
2020-04-28 21:21:53 +0900

    Google Calendar API set up

--- app/src/main/java/com/example/cs125_final_project/MainActivity.java
+++ app/src/main/java/com/example/cs125_final_project/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.cs125_final_project;
 
 import android.content.Intent;
-import android.drm.DrmStore;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
