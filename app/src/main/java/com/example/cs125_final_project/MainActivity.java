f3f01961b731422ecdee2519c07bc4e3092f6af0
dl-ux <dl14@illinois.edu>
2020-04-28 15:23:17 -0500

    UI Update and functionality

--- app/src/main/java/com/example/cs125_final_project/MainActivity.java
+++ app/src/main/java/com/example/cs125_final_project/MainActivity.java
@@ -1,31 +1,23 @@
 package com.example.cs125_final_project;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
 
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.ArrayList;
+import com.google.android.material.navigation.NavigationView;
 
 
-public class MainActivity extends AppCompatActivity {
-    private ArrayList<String> tasks;
-    // This is where the tasks are added
-    public ArrayAdapter<String> itemsAdapter;
-    private ListView task_list;
+
+
+//Navigation from Coding in Flow on YouTube
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
-    EditText tru;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,24 +28,38 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ToDoFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_todo);
+        }
+    }
 
-        //Creates list to add tasks
-        task_list = findViewById(R.id.task_list);
-        tasks = new ArrayList<>();
-        itemsAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, tasks);
-        task_list.setAdapter(itemsAdapter);
-
-        // implements remove
-        removeTask();
-        //implements add
-        addTask();
-        taskDescription();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_todo:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ToDoFragment()).commit();
+                break;
+            case R.id.nav_calendar:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CalendarFragment()).commit();
+                break;
+            case R.id.nav_event:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new EventFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -63,50 +69,5 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
-    }
-
-    // When held, the item is deleted (maybe change this to a button next to each task?)
-    private void removeTask() {
-        task_list.setOnItemLongClickListener(
-                new AdapterView.OnItemLongClickListener() {
-                    @Override
-                    public boolean onItemLongClick(AdapterView<?> adapter,
-                                                   View item, int pos, long id) {
-                        // Remove task
-                        tasks.remove(pos);
-                        // Refresh
-                        itemsAdapter.notifyDataSetChanged();
-                        // It worked
-                        return true;
-                    }
-
-                });
-    }
-    //Goes to secondary activity: TaskDescriptionActivity
-    private void addTask() {
-        FloatingActionButton fab = findViewById(R.id.add_task);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText task = findViewById(R.id.editText);
-                final String taskOnClick = task.getText().toString();
-                if (taskOnClick != null && taskOnClick.length() != 0) {
-                    itemsAdapter.add(taskOnClick);
-                    itemsAdapter.notifyDataSetChanged();
-                    task.getText().clear();
-                }
-            }
-        });
-    }
-    private void taskDescription() {
-        task_list.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapter,
-                                                   View item, int pos, long id) {
-                        Intent intent = new Intent(getApplicationContext(), TaskDescriptionActivity.class);
-                        startActivity(intent);
-                    }
-                });
     }
 }
