a9befcde0b2b684c4f841829cfe4481a6113a480
dl-ux <dl14@illinois.edu>
2020-05-04 01:06:03 -0500

    update

--- app/src/main/java/com/example/cs125_final_project/ToDoFragment.java
+++ app/src/main/java/com/example/cs125_final_project/ToDoFragment.java
@@ -1,12 +1,18 @@
 package com.example.cs125_final_project;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -16,13 +22,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
+import static android.content.Context.MODE_PRIVATE;
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 
 public class ToDoFragment extends Fragment {
+
+    private ArrayList<String> menuItems;
+
     public ToDoFragment() {
         //Empty
     }
@@ -33,9 +48,17 @@ public class ToDoFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_todo, container, false);
 
-        final ArrayList<String> menuItems = new ArrayList<>();
+        loadData();
+
+        Button buttonSave = view.findViewById(R.id.button_save);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+                Toast.makeText(getActivity(), "List Saved!", Toast.LENGTH_SHORT).show();
+            }
+        });
         ListView listView = view.findViewById(R.id.task_list);
-        final EditText task = view.findViewById(R.id.editText);
 
         final ArrayAdapter<String> listViewAdapter = new ArrayAdapter<>(
                 getActivity(),
@@ -48,18 +71,28 @@ public class ToDoFragment extends Fragment {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String taskOnClick = task.getText().toString();
-                if (taskOnClick != null && taskOnClick.length() != 0) {
-                    menuItems.add(taskOnClick);
-                    listViewAdapter.notifyDataSetChanged();
-                    task.getText().clear();
-                } else {
-                    Toast.makeText(getActivity(), "Enter Something!", Toast.LENGTH_SHORT).show();
-                }
+                AlertDialog.Builder todoTaskBuilder = new AlertDialog.Builder(getActivity());
+                todoTaskBuilder.setTitle("Add a Task");
+                todoTaskBuilder.setMessage("Describe it");
+                final EditText todoET = new EditText(getActivity());
+                todoTaskBuilder.setView(todoET);
+                todoTaskBuilder.setPositiveButton("Add Task", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String todoTaskInput = todoET.getText().toString();
+                        if (todoTaskInput != null || todoTaskInput.length() != 0) {
+                            menuItems.add(todoTaskInput);
+                            Toast.makeText(getActivity(), "Remember to Save!", Toast.LENGTH_SHORT).show();
+                            listViewAdapter.notifyDataSetChanged();
+                            saveData();
+                        }
+                    }
+                });
+
+                todoTaskBuilder.setNegativeButton("Cancel", null);
+                todoTaskBuilder.create().show();
             }
         });
-
-
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
                     @Override
                     public boolean onItemLongClick(AdapterView<?> adapter,
@@ -73,7 +106,28 @@ public class ToDoFragment extends Fragment {
                     }
         });
 
+
         return view;
     }
+    private void saveData() {
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences(
+                "shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(menuItems);
+        editor.putString("task list", json);
+        editor.apply();
+    }
+    private void loadData() {
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences(
+                "shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<ArrayList>() {}.getType();
+        menuItems = gson.fromJson(json, type);
 
+        if (menuItems == null) {
+            menuItems = new ArrayList<>();
+        }
+    }
 }
