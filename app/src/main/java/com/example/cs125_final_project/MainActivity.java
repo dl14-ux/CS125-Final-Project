bc70b84c0e70a54cd71d29f006f504b8fda9838e
hollylee <jungmin3@illinois.edu>
2020-05-04 21:00:28 +0900

    Merge issues

--- app/src/main/java/com/example/cs125_final_project/MainActivity.java
+++ app/src/main/java/com/example/cs125_final_project/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.cs125_final_project;
 
-
-
 import android.os.Bundle;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
@@ -11,8 +9,6 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
-
-
 
 
 //Navigation from Coding in Flow on YouTube
